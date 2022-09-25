85189466d5ba4150242a84643731fd799a0e6e3e
FernandoFraile <82083270+FernandoFraile@users.noreply.github.com>
2022-09-25 09:45:11 +0200

    Primera versión de la práctica

---
+++ src/Servidor.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.net.*;
+
+public class Servidor {
+    public static void main(String[] args){
+        try{
+
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+}
