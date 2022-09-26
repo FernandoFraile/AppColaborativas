edf4e6d7ce7fab28d3d4f58e35fca5df1f84320d
FernandoFraile <82083270+FernandoFraile@users.noreply.github.com>
2022-09-26 16:42:38 +0200

    Primera versión de la práctica

--- src/Cliente.java
+++ src/Cliente.java
@@ -3,11 +3,27 @@ import java.net.*;
 
 public class Cliente{
     public static void main(String[] args){
+        MulticastSocket sock = null;
+        byte[] buffer = new byte[100];
+        int[] secuencia = new int[15]; //Variable para almacenar la secuencia de cada cliente
+        int min_val=1;
+        int max_val=90;
         try{
+            InetAddress group = InetAddress.getByName("225.0.0.100");
+            sock = new MulticastSocket(6789);
+            sock.joinGroup(group);
 
-        }
-        catch(Exception e){
-            System.out.println(e.getMessage());
+                DatagramPacket mIn = new DatagramPacket(buffer,buffer.length);
+                sock.receive(mIn);
+                System.out.println("Mensaje recibido" + new String(mIn.getData()));
+
+
+        }catch (SocketException e){
+            System.out.println("Socket: " + e.getMessage());
+        }catch (IOException e){
+            System.out.println("IO: " + e.getMessage());
+        }finally {
+            if(sock!= null) sock.close();
         }
     }
 
--- src/Servidor.java
+++ src/Servidor.java
@@ -3,11 +3,23 @@ import java.net.*;
 
 public class Servidor {
     public static void main(String[] args){
+        MulticastSocket sock = null;
+        String mensaje = "Hola";
         try{
-
+            InetAddress group = InetAddress.getByName("225.0.0.100");
+            sock = new MulticastSocket(6789);
+            sock.joinGroup(group);
+            byte[] mBytes = mensaje.getBytes();
+            //DatagramPacket mOut = new DatagramPacket(mBytes,mensaje.length(),group,sock.getLocalPort());
+            DatagramPacket mOut = new DatagramPacket(mBytes,mensaje.length(),group,6789);
+            sock.send(mOut);
         }
-        catch(Exception e){
-            System.out.println(e.getMessage());
+        catch (SocketException e){
+            System.out.println("Socket: " + e.getMessage());
+        }catch (IOException e){
+            System.out.println("IO: " + e.getMessage());
+        }finally {
+            if(sock!= null) sock.close();
         }
     }
 
